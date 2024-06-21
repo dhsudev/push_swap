9a438c88a6132a2cc6edc9210803f2f71727eb4a
dhsudev <lua@gnomo.net>
2024-06-21 17:53:25 +0200

    Solved segfault when adding to the struct

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: ltrevin- <ltrevin-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/16 17:41:32 by ltrevin-          #+#    #+#             */
-/*   Updated: 2024/06/16 19:12:46 by ltrevin-         ###   ########.fr       */
+/*   Updated: 2024/06/21 16:04:24 by ltrevin-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,12 +16,13 @@
 int	main(int argc, char **argv)
 {
 	t_stack	*a;
-	t_stack *b;
+//	t_stack *b;
 
 	a = NULL;
-	b = NULL;
+ //	b = NULL;
 	// Args checking
 	if (argc < 2)
 		return (write(1, "ERROR", 5));
+	print_stack(a);
 	init_stack(argv, argc, a);
 }
