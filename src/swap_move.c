68182744e2f8c96dfe8a695b4a4a66a2b1fb52c7
dhsudev <lua@gnomo.net>
2024-07-01 16:30:38 +0200

    Added moves

--- src/swap_move.c
+++ src/swap_move.c
@@ -6,18 +6,18 @@
 /*   By: ltrevin- <ltrevin-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/30 18:52:45 by ltrevin-          #+#    #+#             */
-/*   Updated: 2024/06/30 19:16:30 by ltrevin-         ###   ########.fr       */
+/*   Updated: 2024/07/01 16:00:15 by ltrevin-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/push_swap.h"
 
-void swap(t_stack **stack)
+void	swap(t_stack **stack)
 {
-	t_stack *first;
-	t_stack *second;
-	
-	if(!stack || !(*stack)->next)
+	t_stack	*first;
+	t_stack	*second;
+
+	if (!stack || !(*stack)->next)
 		return ;
 	first = *stack;
 	second = (*stack)->next;
@@ -28,21 +28,21 @@ void swap(t_stack **stack)
 	*stack = second;
 }
 
-void	sa(t_stack_node	**a, bool checker)
+void	sa(t_stack **a, int checker)
 {
 	swap(a);
 	if (!checker)
 		write(1, "sa\n", 3);
 }
 
-void	sb(t_stack_node **b, bool checker)
+void	sb(t_stack **b, int checker)
 {
 	swap(b);
 	if (!checker)
 		write(1, "sb\n", 3);
 }
 
-void	ss(t_stack_node **a, t_stack_node **b, bool checker)
+void	ss(t_stack **a, t_stack **b, int checker)
 {
 	swap(a);
 	swap(b);
